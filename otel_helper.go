d9d291082e0e9e179e4a1be307f30457ec924639
Prabhat Sharma <hi.prabhat@gmail.com>
2022-12-07 22:49:06 -0800

    refactored

---
+++ otel_helper.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+)
+
+func initTracer() *sdktrace.TracerProvider {
+	OTEL_OTLP_HTTP_ENDPOINT := os.Getenv("OTEL_OTLP_HTTP_ENDPOINT")
+
+	if OTEL_OTLP_HTTP_ENDPOINT == "" {
+		OTEL_OTLP_HTTP_ENDPOINT = "otel.dev2.zinclabs.dev"
+	}
+
+	otlptracehttp.NewClient()
+
+	otlpHTTPExporter, err := otlptracehttp.New(context.TODO(),
+		// otlptracehttp.WithInsecure(),
+		otlptracehttp.WithEndpoint(OTEL_OTLP_HTTP_ENDPOINT),
+		otlptracehttp.WithHeaders(map[string]string{
+			"Authorization": "Basic YWRtaW46Q29tcGxleHBhc3MjMTIz",
+		}),
+	)
+
+	if err != nil {
+		fmt.Println("Error creating HTTP OTLP exporter: ", err)
+	}
+
+	res := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		// the service name used to display traces in backends
+		semconv.ServiceNameKey.String("otel1-gin-gonic"),
+	)
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithBatcher(otlpHTTPExporter),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
+}
